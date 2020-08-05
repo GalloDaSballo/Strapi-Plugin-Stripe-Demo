a4da115058b17cfbbd918acadb36e4ab746609a4
Entreprenerd <alex@entreprenerd.xyz>
2020-08-05 12:31:18 +0200

    Routes are ready

--- plugins/stripe/controllers/stripe.js
+++ plugins/stripe/controllers/stripe.js
@@ -21,5 +21,51 @@ module.exports = {
     ctx.send({
       message: 'ok'
     });
+  },
+
+  updateSettings: async (ctx) => {
+    const {user} = ctx.state
+    const {pk} = ctx.request.body
+
+    //Ensure user is admin
+    if(user.roles[0].id != 1){
+      return ctx.unauthorized("Only administrators allowed!")
+    }
+
+    if(!pk){
+      return ctx.throw(400, "Please provide a private key")
+    }
+
+    const pluginStore = strapi.store({
+      environment: strapi.config.environment,
+      type: 'plugin',
+      name: 'stripe' //I would recommend prefixing entreprenerd-stripe
+    })
+
+    const result = await pluginStore.set({ key: 'pk', value: pk })
+
+    ctx.send({
+      result
+    })
+  },
+
+  retrieveSettings: async (ctx) => {
+    const {user} = ctx.state
+
+    if(user.roles[0].id != 1){
+      return ctx.unauthorized("Only admins")
+    }
+
+    const pluginStore = strapi.store({
+      environment: strapi.config.environment,
+      type: 'plugin',
+      name: 'stripe'
+    })
+
+    const pk = await pluginStore.get({ key: 'pk' })
+
+    ctx.send({
+      pk: pk ? pk : ''
+    })
   }
 };
